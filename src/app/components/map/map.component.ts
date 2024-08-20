ffe92c4712d1851dbf47f323058719ed41d2faf2
Jesus <jesusperez_14@hotmail.com>
2024-08-20 13:29:32 +0200

    Map implemented

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -9,10 +9,10 @@ import { geoMercator, geoPath } from 'd3-geo';
 })
 export class MapComponent implements OnInit {
   private svg!: d3.Selection<SVGSVGElement, unknown, null, undefined>;
-  private width = 960;
-  private height = 600;
+  private width = 1000;
+  private height = 800;
 
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
     this.createSvg();
@@ -23,18 +23,20 @@ export class MapComponent implements OnInit {
     this.svg = d3.select(this.el.nativeElement)
       .select('.map-container')
       .append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height);
+      .attr('width', '100%')  // Make responsive
+      .attr('height', '99%') // Make responsive
+      .attr('viewBox', `0 0 ${this.width} ${this.height}`)
+      .attr('preserveAspectRatio', 'xMidYMid meet'); // Maintain aspect ratio
   }
 
   private drawMap(): void {
     const projection = geoMercator()
-      .scale(150)
-      .translate([this.width / 2, this.height / 1.5]);
+      .scale(180)
+      .translate([this.width / 2, this.height / 1.39]);
 
     const path = geoPath().projection(projection);
 
-    d3.json('assets/world.geojson').then((data: any) => {
+    d3.json('assets/custom110m.geo.json').then((data: any) => {
       if (!data) {
         console.error('No data loaded');
         return;
@@ -46,19 +48,35 @@ export class MapComponent implements OnInit {
         .enter()
         .append('path')
         .attr('d', (d: any) => path(d) as string)
-        .attr('fill', '#cccccc')
-        .attr('stroke', '#333333')
-        .attr('stroke-width', 0.5)
+        .attr('fill', '#BDC3C7')
+        .attr('stroke', '#747474')
+        .attr('stroke-width', 1)
+        .on('mouseover', function (event: any, d: any) {
+          d3.select(this)
+            .transition()
+            .duration(100)
+            .attr('fill', '#8A8A8A')  // Hover color
+            .attr('filter', 'drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5))');
+        })
+        .on('mouseout', function (event: any, d: any) {
+          d3.select(this)
+            .transition()
+            .duration(200)
+            .attr('fill', '#BDC3C7')  // Original color
+            .attr('filter', 'none');
+        })
         .on('click', (event: any, d: any) => {
           this.onCountrySelect(d.properties.name);
         });
-    }).catch((error: any) => {
-      console.error('Error loading GeoJSON data', error);
     });
+
+    
   }
 
+
+  
   private onCountrySelect(country: string): void {
     console.log('Country selected:', country);
-    // Aquí puedes navegar a la página de cervezas del país seleccionado
+    // Navegar a la página de cervezas del país seleccionado o realizar otra acción
   }
 }
