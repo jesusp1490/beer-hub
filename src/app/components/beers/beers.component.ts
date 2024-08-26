a2700403d231793ad02ab8da722b1103d11c8b3a
Jesus <jesusperez_14@hotmail.com>
2024-08-26 12:40:01 +0200

    Beer component

--- src/app/components/beers/beers.component.ts
+++ src/app/components/beers/beers.component.ts
@@ -10,82 +10,76 @@ import { Brand } from '../country/brand.interface';
   templateUrl: './beers.component.html',
   styleUrls: ['./beers.component.scss']
 })
-
 export class BeersComponent implements OnInit {
   brandName: string = '';
   beers: Beer[] = [];
+  visibleBeers: Beer[] = [];
   page: number = 0;
-  pageSize: number = 6; // Número de cervezas por página
-  loading: boolean = false;
-  totalBeers: number = 0;
+  pageSize: number = 5; // Número de cervezas por página
+  private countryId: string = '';
+  private brandId: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private firestore: AngularFirestore,
     private router: Router
-  ) { }
+  ) {
+    this.countryId = this.route.snapshot.paramMap.get('country') || '';
+    this.brandId = this.route.snapshot.paramMap.get('brandId') || '';
+  }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const countryId = params.get('country');
-      const brandId = params.get('brandId');
-      console.log('Country ID:', countryId);
-      console.log('Brand ID:', brandId);
-
-      if (countryId && brandId) {
-        this.loadBrandData(brandId);
-        this.loadBeers(countryId, brandId);
-      }
-    });
+    this.loadBrandData(this.brandId);
+    this.loadBeers(this.countryId, this.brandId);
   }
 
   private loadBrandData(brandId: string): void {
     this.firestore.collection<Brand>('brands').doc(brandId).valueChanges().subscribe(brand => {
       if (brand) {
         this.brandName = brand.name;
+      } else {
+        console.error('Brand not found');
       }
     });
   }
 
   private loadBeers(countryId: string, brandId: string): void {
-    this.loading = true;
     this.firestore.collection<Beer>('beers', ref => ref
       .where('countryId', '==', countryId)
-      .where('brandId', '==', brandId)
-      .orderBy('name')
-      .startAfter(this.page * this.pageSize)
-      .limit(this.pageSize)
-    ).valueChanges().subscribe(beers => {
-      this.beers = beers;
-      this.totalBeers = beers.length;
-      this.loading = false;
-    });
+      .where('brandId', '==', brandId))
+      .valueChanges()
+      .subscribe(beers => {
+        this.beers = beers;
+        this.updateVisibleBeers();
+      });
   }
 
-  currentBeers(): Beer[] {
+  private updateVisibleBeers(): void {
     const start = this.page * this.pageSize;
-    return this.beers.slice(start, start + this.pageSize);
-  }
-
-  selectBeer(beerId: string): void {
-    this.router.navigate([`/country/${this.route.snapshot.paramMap.get('country')}/brands/${this.route.snapshot.paramMap.get('brandId')}/beers/${beerId}`]);
+    const end = start + this.pageSize;
+    this.visibleBeers = this.beers.slice(start, end);
   }
 
   prevPage(): void {
     if (this.page > 0) {
       this.page--;
-      this.loadBeers(this.route.snapshot.paramMap.get('country')!, this.route.snapshot.paramMap.get('brandId')!);
+      this.updateVisibleBeers();
     }
   }
 
   nextPage(): void {
-    if (this.page * this.pageSize + this.pageSize < this.totalBeers) {
+    if ((this.page + 1) * this.pageSize < this.beers.length) {
       this.page++;
-      this.loadBeers(this.route.snapshot.paramMap.get('country')!, this.route.snapshot.paramMap.get('brandId')!);
+      this.updateVisibleBeers();
     }
   }
 
-  hasMoreBeers(): boolean {
-    return this.page * this.pageSize + this.pageSize < this.totalBeers;
+  selectBeer(beerId: string): void {
+    const route = `/country/${this.countryId}/brands/${this.brandId}/beers/${beerId}`;
+    this.router.navigate([route]);
+  }
+
+  get hasMoreBeers(): boolean {
+    return (this.page + 1) * this.pageSize < this.beers.length;
   }
 }
