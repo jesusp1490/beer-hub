11e6df5f464e7e13ac0c67d11ee26a778d6958e4
Jesus <jesusperez_14@hotmail.com>
2024-09-05 21:14:07 +0200

    Slick carrousel implementing

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Country } from './country.interface';
 import { Brand } from './brand.interface';
+import * as $ from 'jquery';
+import 'slick-carousel';
+import { ChangeDetectorRef } from '@angular/core'; // Importar ChangeDetectorRef
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit {
+export class CountryComponent implements OnInit, AfterViewInit, AfterViewChecked {
   country$: Observable<Country | undefined>;
   countryName: string = '';
   countryFlagUrl: string = '';
   brands: Brand[] = [];
-  private countryId: string = ''; 
+  private countryId: string = '';
   page: number = 0;
-  pageSize: number = 10; 
+  pageSize: number = 10;
   visibleBrands: Brand[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private firestore: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef // Inyectar ChangeDetectorRef
   ) {
     this.countryId = this.route.snapshot.paramMap.get('country') || '';
     this.country$ = this.firestore.doc<Country>(`countries/${this.countryId}`).valueChanges();
@@ -51,13 +55,18 @@ export class CountryComponent implements OnInit {
       .subscribe(brands => {
         this.brands = brands;
         this.updateVisibleBrands();
+        console.log('Visible brands:', this.visibleBrands);
+        if (this.brands.length > 0) {
+          this.initializeSlick();
+          this.cdr.detectChanges(); // Forzar detección de cambios
+        } else {
+          console.error('No brands found');
+        }
       });
   }
 
-  private updateVisibleBrands(): void {
-    const start = this.page * this.pageSize;
-    const end = start + this.pageSize;
-    this.visibleBrands = this.brands.slice(start, end);
+  updateVisibleBrands(): void {
+    this.visibleBrands = this.brands.slice(this.page * this.pageSize, (this.page + 1) * this.pageSize);
   }
 
   prevPage(): void {
@@ -76,7 +85,7 @@ export class CountryComponent implements OnInit {
 
   selectBrand(brandId: string): void {
     const route = `/country/${this.countryId}/brands/${brandId}/beers`;
-    console.log('Navigating to:', route); // Verifica la URL generada
+    console.log('Navigating to:', route);
     this.router.navigate([route]);
   }
 
@@ -90,5 +99,54 @@ export class CountryComponent implements OnInit {
       rows.push(this.visibleBrands.slice(i, i + 5));
     }
     return rows;
+  }
+
+  ngAfterViewInit(): void {
+    this.initializeSlick();
+  }
+
+  ngAfterViewChecked(): void {
+    this.initializeSlick();
+  }
+
+  private initializeSlick(): void {
+    const $carouselTrack = $('.carousel-track');
+
+    if ($carouselTrack.length && !$carouselTrack.hasClass('slick-initialized')) {
+      setTimeout(() => {
+        $carouselTrack.slick({
+          infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow: '.carousel-button.left',
+          nextArrow: '.carousel-button.right',
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+
+        console.log('Carousel items:', $carouselTrack.children().length);
+      }, 100); // Ajusta el delay según sea necesario
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import * as $ from 'jquery';
+import 'slick-carousel';
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
