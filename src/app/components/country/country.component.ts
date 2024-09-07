252a42a5d7a4dd6fed0c008216ecb9cbb5543d98
Jesus <jesusperez_14@hotmail.com>
2024-09-07 17:37:02 +0200

    Country Component remake

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,35 +1,29 @@
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Country } from './country.interface';
 import { Brand } from './brand.interface';
-import { SlickCarouselComponent } from 'ngx-slick-carousel';
-import * as $ from 'jquery';
-import 'slick-carousel';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.scss']
 })
-export class CountryComponent implements OnInit, AfterViewInit {
+export class CountryComponent implements OnInit {
   country$: Observable<Country | undefined>;
   countryName: string = '';
   countryFlagUrl: string = '';
   brands: Brand[] = [];
-  private countryId: string = '';
+  private countryId: string = ''; 
   page: number = 0;
-  pageSize: number = 10;
+  pageSize: number = 10; 
   visibleBrands: Brand[] = [];
-
-
 
   constructor(
     private route: ActivatedRoute,
     private firestore: AngularFirestore,
-    private router: Router,
-    private cdr: ChangeDetectorRef
+    private router: Router
   ) {
     this.countryId = this.route.snapshot.paramMap.get('country') || '';
     this.country$ = this.firestore.doc<Country>(`countries/${this.countryId}`).valueChanges();
@@ -57,17 +51,13 @@ export class CountryComponent implements OnInit, AfterViewInit {
       .subscribe(brands => {
         this.brands = brands;
         this.updateVisibleBrands();
-        console.log('Visible brands:', this.visibleBrands);
-        if (this.brands.length > 0) {
-          this.cdr.detectChanges(); // Forzar detección de cambios
-        } else {
-          console.error('No brands found');
-        }
       });
   }
 
-  updateVisibleBrands(): void {
-    this.visibleBrands = this.brands.slice(this.page * this.pageSize, (this.page + 1) * this.pageSize);
+  private updateVisibleBrands(): void {
+    const start = this.page * this.pageSize;
+    const end = start + this.pageSize;
+    this.visibleBrands = this.brands.slice(start, end);
   }
 
   prevPage(): void {
@@ -86,7 +76,7 @@ export class CountryComponent implements OnInit, AfterViewInit {
 
   selectBrand(brandId: string): void {
     const route = `/country/${this.countryId}/brands/${brandId}/beers`;
-    console.log('Navigating to:', route);
+    console.log('Navigating to:', route); // Verifica la URL generada
     this.router.navigate([route]);
   }
 
@@ -100,54 +90,5 @@ export class CountryComponent implements OnInit, AfterViewInit {
       rows.push(this.visibleBrands.slice(i, i + 5));
     }
     return rows;
-  }
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.initializeSlick();
-    }, 100); // Asegúrate de que el contenido esté disponible
-  }
-
-
-  private initializeSlick(): void {
-    const $carouselTrack = $('.carousel-track');
-
-    if ($carouselTrack.length && !$carouselTrack.hasClass('slick-initialized')) {
-      setTimeout(() => {
-        $carouselTrack.slick({
-          infinite: true,
-          slidesToShow: 5,
-          slidesToScroll: 3,
-          centerMode: true,
-          centerPadding: '10px', // Ajusta según sea necesario
-          dots: true, // Habilita los puntos de navegación,
-          prevArrow: '.carousel-button.left',
-          nextArrow: '.carousel-button.right',
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-        });
-      }, 100);
-    }
   }
 }
