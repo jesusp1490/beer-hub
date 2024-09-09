ef3e0709f9a035152df6be620cae5999af2277d7
Jesus <jesusperez_14@hotmail.com>
2024-09-09 16:09:52 +0200

    fixing styles

--- src/app/components/beers/beers.component.ts
+++ src/app/components/beers/beers.component.ts
@@ -1,29 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Location } from '@angular/common';
 import { Beer } from './beers.interface';
 import { Brand } from '../country/brand.interface';
+// import * as $ from 'jquery';
+// import 'slick-carousel';
 
 @Component({
   selector: 'app-beers',
   templateUrl: './beers.component.html',
   styleUrls: ['./beers.component.scss']
 })
-export class BeersComponent implements OnInit {
+export class BeersComponent implements OnInit, AfterViewInit {
   brandName: string = '';
   beers: Beer[] = [];
   visibleBeers: Beer[] = [];
   page: number = 0;
-  pageSize: number = 5; // Número de cervezas por página
+  pageSize: number = 5; 
   private countryId: string = '';
   private brandId: string = '';
 
-  // Configuración para Slick Slider
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore,
+    private router: Router,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.countryId = params.get('country') || '';
+      this.brandId = params.get('brandId') || '';
+      this.loadBrandData(this.brandId);
+      this.loadBeers(this.countryId, this.brandId);
+    });
+  }
+
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.initializeSlick();
-    }, 100); // Asegúrate de que el contenido esté disponible
+    }, 200); // Ensure the content is available
   }
 
   private initializeSlick(): void {
@@ -31,23 +48,23 @@ export class BeersComponent implements OnInit {
 
     if ($carouselTrack.length && !$carouselTrack.hasClass('slick-initialized')) {
       setTimeout(() => {
-        const slidesToShow = this.beers.length < 5 ? this.beers.length : 5; // Mostrar el número de cervezas disponibles, hasta un máximo de 5
-        const slidesToScroll = this.beers.length < 3 ? 1 : 3; // Desplazar 1 si hay menos de 3 cervezas
+        const slidesToShow = this.beers.length < 5 ? this.beers.length : 5;
+        const slidesToScroll = this.beers.length < 3 ? 1 : 3;
 
         $carouselTrack.slick({
-          infinite: this.beers.length > 1, // Desactivar infinite si solo hay 1 cerveza
+          infinite: true,
           slidesToShow: slidesToShow,
           slidesToScroll: slidesToScroll,
-          centerMode: this.beers.length > 1, // Solo centrar si hay más de 1 cerveza
+          centerMode: true,
           centerPadding: '0px',
-          dots: this.beers.length > 1, // Desactivar puntos de navegación si solo hay 1 cerveza
+          dots: true,
           prevArrow: '.carousel-button.left',
           nextArrow: '.carousel-button.right',
           responsive: [
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: Math.min(slidesToShow, 3), // Ajustar según el tamaño de pantalla
+                slidesToShow: Math.min(slidesToShow, 3),
                 slidesToScroll: Math.min(slidesToScroll, 1)
               }
             },
@@ -71,34 +88,23 @@ export class BeersComponent implements OnInit {
     }
   }
 
-  constructor(
-    private route: ActivatedRoute,
-    private firestore: AngularFirestore,
-    private router: Router,
-    private location: Location
-  ) { }
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.countryId = params.get('country') || '';
-      this.brandId = params.get('brandId') || '';
-      this.loadBrandData(this.brandId);
-      this.loadBeers(this.countryId, this.brandId);
-    });
-  }
-
   goBack(): void {
-    this.location.back(); // Navega hacia la página anterior
+    this.location.back(); // Navigate to the previous page
   }
 
   private loadBrandData(brandId: string): void {
-    this.firestore.collection<Brand>('brands').doc(brandId).valueChanges().subscribe(brand => {
-      if (brand) {
-        this.brandName = brand.name;
-      } else {
-        console.error('Brand not found');
+    this.firestore.collection<Brand>('brands').doc(brandId).valueChanges().subscribe(
+      brand => {
+        if (brand) {
+          this.brandName = brand.name;
+        } else {
+          console.error('Brand not found');
+        }
+      },
+      error => {
+        console.error('Error loading brand data:', error);
       }
-    });
+    );
   }
 
   private loadBeers(countryId: string, brandId: string): void {
@@ -106,10 +112,15 @@ export class BeersComponent implements OnInit {
       .where('countryId', '==', countryId)
       .where('brandId', '==', brandId))
       .valueChanges()
-      .subscribe(beers => {
-        this.beers = beers;
-        this.updateVisibleBeers();
-      });
+      .subscribe(
+        beers => {
+          this.beers = beers;
+          this.updateVisibleBeers();
+        },
+        error => {
+          console.error('Error loading beers:', error);
+        }
+      );
   }
 
   private updateVisibleBeers(): void {
@@ -119,7 +130,7 @@ export class BeersComponent implements OnInit {
   }
 
   selectBeer(beerId: string): void {
-    console.log('Selecting beer with ID:', beerId); // Verifica que el ID es correcto
+    console.log('Selecting beer with ID:', beerId); // Verify the ID is correct
     const route = `/country/${this.countryId}/brands/${this.brandId}/beers/${beerId}`;
     this.router.navigate([route]);
   }
