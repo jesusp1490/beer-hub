d113c42da5b46c225dcacc307301ca6ece77a168
Jesus <jesusperez_14@hotmail.com>
2024-09-27 14:45:20 +0200

    About Us component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrls: ['./about.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatTabsModule]
 })
 export class AboutComponent {
+  teamMembers = [
+    { name: 'Jesús Pérez', role: 'Founder & Beer Enthusiast', image: 'https://firebasestorage.googleapis.com/v0/b/beer-hub.appspot.com/o/profilePictures%2FolUaCy9gcpSwKiWGLA42KWbaedj2?alt=media&token=734e3df4-7794-4c47-bf6f-a7f3971ff043' },
+    { name: 'NukeDev', role: 'Head of Content', image: 'https://firebasestorage.googleapis.com/v0/b/beer-hub.appspot.com/o/profilePictures%2FUqRBL0iHpegayh3gmcR93kg1bnq2?alt=media&token=a8bd80d4-cacf-4d36-a18d-32c163f71019' },
+    { name: 'Mike Johnson', role: 'Chief Tasting Officer', image: 'https://firebasestorage.googleapis.com/v0/b/beer-hub.appspot.com/o/images%2Fmisc%2Fmedium-shot-man-celebrating-new-year-s-eve.webp?alt=media&token=b47e359b-a1b2-4706-8ccf-0f6e78b6dd45' }
+  ];
 
+  constructor() { }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule
+  ]
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      // Here you would typically send the form data to your backend
+    }
+  }
 }
+
