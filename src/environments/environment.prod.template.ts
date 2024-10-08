c91930d0411c402dccabff6b2466d0bbf1c48cc3
Jesus <jesusperez_14@hotmail.com>
2024-10-08 13:48:44 +0200

    env

---
+++ src/environments/environment.prod.template.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3d7lAHBMiDDSAHYWQhEZiDEfmFy1NxjE",
+    authDomain: "beer-hub-c7f53.firebaseapp.com",
+    projectId: "beer-hub",
+    storageBucket: "beer-hub.appspot.com",
+    messagingSenderId: "619627938453",
+    appId: "1:619627938453:web:5d2856ca1699587a5ad19c"
+  }
+};
